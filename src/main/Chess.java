00eefc41b965e9fe7ab1b4181a037b63dd28c229
52000246 <52000246@0355S-S-2090N94.sdcs.local>
2018-05-25 13:36:02 -0700

    Final test of git via netbeans.

--- src/main/Chess.java
+++ src/main/Chess.java
@@ -3,14 +3,6 @@ package main;
 public class Chess {
     public static void main(String[] args) {
         Board board = new Board();
-        /*Board.getIndex(1, 1).move();
-        board.getBoard();
-        Board.getIndex(3, 1).move();
-        board.getBoard();
-        Board.getIndex(4, 1).move();
-        board.getBoard();
-        Board.getIndex(5, 1).move();
-        board.getBoard();*/
     }
     
 }
